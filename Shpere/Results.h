ea070228b22a6ff27dc661caf0050be110685a0d
Nick-Mikrou <mikrounick@gmail.com>
2021-06-27 14:14:19 +0300

    This class retains the results

---
+++ Shpere/Results.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Result
+{
+	private:
+		bool isIntersected;
+		float distance;
+
+	public:
+		Result(const bool isIntersected, const float distance)				
+			:isIntersected(isIntersected), distance(distance)
+		{}																			
+
+		inline bool GetisIntersected() const noexcept { return isIntersected; }	
+		inline float GetDistance()	   const noexcept { return distance; }				
+};
